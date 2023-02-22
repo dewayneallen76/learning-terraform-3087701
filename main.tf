a20f1a57faeea4f0b16db414ca10d06e9cf9f8db
Dewayne Allen <dewayneallen76@gmail.com>
2023-02-22 10:08:08 -0500

    correcting error

--- main.tf
+++ main.tf
@@ -87,11 +87,9 @@ module "blog_sg" {
   version = "4.17.1"
   name    = "blog"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
-
+  vpc_id = data.aws_vpc.default.id
   ingress_rules        = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules        = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }

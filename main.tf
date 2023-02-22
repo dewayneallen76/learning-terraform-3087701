ccd78962f8c00c2d58a0cbc0d6a9209c60b84470
Dewayne Allen <dewayneallen76@gmail.com>
2023-02-22 11:05:23 -0500

    not sure why this keeps failing

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+  
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,9 +46,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   
+  name    = "blog"
   vpc_id  = module.vpc.public_subnets[0]
   
-  name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
